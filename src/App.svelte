bd8e37d085d87d213ff8cb4c75b2801cf1f488fc
WSL: Julian Toscani <julian_toscani@gmail.com>
2021-04-15 23:15:27 +0200

    computededs and events in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,38 @@
 <!-- App.svelte -->
 <script lang="typescript">
-  import Nested from "./Nested.svelte";
+  let count = 0;
+
+  function handleClick() {
+    count += 1;
+  }
+
+  // Computed Prop
+  $: doubled = count * 2;
+
+  function logCount(count: number) {
+    console.log(`The count is ${count}`);
+  }
+
+  // Computed Prop
+  $: logCount(count);
 </script>
 
 <div class="App">
-  <header class="App-header" />
+  <header class="App-header">
+    <a
+      class="App-link"
+      href="https://svelte.dev"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      Learn Svelte
+    </a>
+  </header>
 
   <main>
-    <Nested answer={"42"} />
+    <button on:click={handleClick}> Add </button>
+    <span> Counter is : {count}</span>
+    <span>Doubled: {doubled} </span>
   </main>
 </div>
 
